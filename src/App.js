d3f8472f5327f22cc5706d2b72c99d7e7956901d
Himank Pathak <himankpathak@gmail.com>
2019-10-02 22:41:58 +0530

    added addTodo function to create tasks

--- src/App.js
+++ src/App.js
@@ -1,52 +1,69 @@
 import React, { useState } from 'react';
+import Header from './components/layout/Header'
 import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
 
 import './App.css';
 
 function toggleComplete(id, todos, setTodos) {
-  setTodos(
-  todos.map( todo => {
-    if(todo.id === id) {
-      todo.completed = !todo.completed
-    }
-    return todo;
-  }));
+	setTodos(
+		todos.map( todo => {
+			if(todo.id === id) {
+				todo.completed = !todo.completed
+			}
+		return todo;
+	}));
+}
+
+function addTodo(title, todos, setTodos){
+	const newTodo = {
+		id: todos.slice(-1)[0]['id']+1,
+		title,
+		completed: false
+	}
+	setTodos(
+		[...todos, newTodo]
+	);
 }
 
 function delTodo(id, todos, setTodos) {
-  setTodos(
-    [...todos.filter(todo => todo.id !== id)]
-    );
+	setTodos(
+		[...todos.filter(todo => todo.id !== id)]
+	);
 }
 
 function App() {
-  const [todos, setTodos] = useState(
-    [
-      {
-        id: 1,
-        title: 'Complete homework',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Work on project',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'Take dog out for walk',
-        completed: false
-      }
-    ]
-  );
+	const [todos, setTodos] = useState(
+	[
+		{
+			id: 1,
+			title: 'Complete homework',
+			completed: false
+		},
+		{
+			id: 2,
+			title: 'Work on project',
+			completed: false
+		},
+		{
+			id: 3,
+			title: 'Take dog out for walk',
+			completed: false
+		}
+	]
+	);
 
-  return (
-    <div className="App">
-      <Todos todos={todos}
-      toggleComplete={(id) => { toggleComplete(id, todos, setTodos) }} 
-      delTodo={(id) => { delTodo(id, todos, setTodos) }} />
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="container">
+				<Header />
+				<AddTodo addTodo={(title) => {addTodo(title, todos, setTodos) }} />
+				<Todos todos={todos}
+				toggleComplete={(id) => { toggleComplete(id, todos, setTodos) }}
+				delTodo={(id) => { delTodo(id, todos, setTodos) }} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/AddTodo.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+function onSubmitTodo(e, props, title, setTitle) {
+    e.preventDefault();
+    props.addTodo(title);
+    setTitle('');
+}
+
+export default function AddTodo(props) {
+    const [title, setTitle] = useState("");
+
+    return (
+        <form onSubmit={(e)=> {onSubmitTodo(e, props, title, setTitle)}} style={{ display: 'flex' }}>
+            <input 
+                type = "text"
+                name = "title"
+                placeholder = "Add Todo"
+                style = {{ flex:'10', padding: '5px' }}
+                value = {title}
+                onChange = {(e)=> {setTitle(e.target.value)}}
+            />
+            <input 
+                type="submit"
+                value="submit"
+                className="btn"
+                style={{ flex:'1' }}
+            />
+        </form>
+    )
+}
